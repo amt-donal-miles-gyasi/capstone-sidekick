8a709afe5067408b73a640ae1dbcaa2599343adf
fusion97 <donal-miles.gyasi@amalitech.org>
2023-07-03 09:47:38 +0000

    feat: waiting for main

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,9 +2,19 @@ import { Router } from 'express';
 import { adminGenerateLecturers } from '../controllers/adminCreateLecturersController';
 import { adminGenerateStundent } from '../controllers/adminCreatingStudentsController';
 import { emailCheck } from '../middlewares/email-check';
+import {
+  getAllLectures,
+  getAllStudents,
+  getAllUsers,
+  getAllAss,
+} from '../controllers/adminDashboard';
 
 const app = Router();
 
+app.get('/get-users', getAllUsers);
+app.get('/get-lecturers', getAllLectures);
+app.get('/get-students', getAllStudents);
+app.get('/get-assignments', getAllAss);
 app.post('/upload-lecturer', emailCheck, adminGenerateLecturers);
 app.post('/upload-student', adminGenerateStundent);
 
